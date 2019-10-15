4ba4a92976460171aa04bf895c5672c88d8dc137
hwarim_laptop <rmrmhrm@gmail.com>
2019-10-15 13:38:00 +0900

    다시 업로드

---
+++ AircampV3.3/src/main/java/com/aircamp/mapper/MemberMapper.java
@@ -0,0 +1,33 @@
+package com.aircamp.mapper;
+
+import com.aircamp.domain.MemberVO;
+
+public interface MemberMapper {
+	// V0수정 정현 2019-07-31
+
+	// memberjoin
+	public void memberjoin(MemberVO vo);
+
+	// idcheck
+	public Integer idcheck(String id);
+
+	//idread
+	public MemberVO idread(String id);
+	
+	//memberupdate
+	public int memberupdate(MemberVO vo);
+	
+	//memberdelete
+	public int memberdelete(String id);
+	
+
+	// V0수정 화림 2019-07-25
+	// memberjoinAuth 새로운 맴버가 회원가입할 때 auth테이블에 맴버의 권한을 ROLE_MEMBER 입력함
+	public void memberjoinAuth(MemberVO vo);
+	
+	// id 찾기
+	public String findId(MemberVO vo);
+
+	public String findPw(MemberVO vo);
+	
+}
